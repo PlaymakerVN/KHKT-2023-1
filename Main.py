e3199ee87b15179d915bbee8f5aabb5799a662ce
PlaymakerVN <lexuantamle1@gmail.com>
2023-10-26 22:36:26 +0700

    ...

--- Main.py
+++ Main.py
@@ -5,48 +5,70 @@ import requests
 from datetime import datetime as dt
 
 # Host Locate
-hosts_path = "C:\Windows\System32\drivers\etc\hosts"
+# hosts_path = "C:\Windows\System32\drivers\etc\hosts"
+hosts_path = "/etc/hosts"
 # localhost's IP
 redirect = "127.0.0.1"
 
-# server="https://uninhibited-pints.000webhostapp.com/post.php"
+server="https://khkt-lxt.000webhostapp.com/post.php"
 # 
 r = requests.post(url=server,data={'text':'BOT IS STARTED'})
-
 # Websites block
-website_list =["fast.com","text.com"]
+website_list =["anonsop.com","text.com"]
 
-check = input("Y or N:")
+c = input("Y or N:")
+#Function
+def get_ip():
+    r = requests.post(url=server,data={'text':'ip'})
+    a = r.text
+    print(a)
+    # for a in range(len(a)):
+    #     print(a)
+    pass
+get_ip()
+#BLOCK
+def block():
+    r = requests.post(url=server,data={'text':'BLOCKED'})
+    #POST IP
+    for i in range(len(website_list)):
+        print("BLOCKED " + website_list[i])
+        r = requests.post(url=server,data={'text':'IP : '+ website_list[i]})
+    #READ FILE
+    with open(hosts_path, 'r+') as file:
+        content = file.read()
+        for website in website_list:
+            if website in content:
+                pass
+            else:
+                # Apply to host
+                file.write(redirect + " " + website + "\n")
+                file.write(redirect + " " +"www."+ website + "\n")
+    pass
 
-#Main
+#UNBLOCK
+def unblock():
+    r = requests.post(url=server,data={'text':'UNBLOCKED'})
+    with open(hosts_path, "r+") as file:
+        content = file.readlines()
+        file.seek(0)
+        for line in content:
+            if not any(website in line for website in website_list):
+                # Apply to host
+                file.write(line)
+                #POST
+        file.truncate()
+        print("Unblocked !!")
+    pass
+
+
+
+
+#MCORE PROGRAMME
 while True:
-    if check == "y" or check == "Y":
-        print("Working hours...")
-        r = requests.post(url=server,data={'text':'BLOCKED'})
-        #POST IP
-        for i in range(len(website_list)):
-        	r = requests.post(url=server,data={'text':'IP : '+ website_list[i]})
-        with open(hosts_path, 'r+') as file:
-            content = file.read()
-            for website in website_list:
-                if website in content:
-                    pass
-                else:
-                    # Apply to host
-                    file.write(redirect + " " + website + "\n")
-                    file.write(redirect + " " +"www."+ website + "\n")
-        #Success 
-        print("Blocked !!") 
+    if(c == "y" or c == "Y"):
+        block()
         break
-    else:
-        with open(hosts_path, "r+") as file:
-            content = file.readlines()
-            file.seek(0)
-            for line in content:
-                if not any(website in line for website in website_list):
-                    # Apply to host
-                    file.write(line)
-            file.truncate()
-            print("Unblocked !!")
+    if (c == "n" or c =="N"):
+        unblock()
         break
 
